1b8ede8755c05dc0b90039d3023bff9eae874901
Salisahmaddd <160557634+aminssmilee@users.noreply.github.com>
2026-06-27 07:31:30 +0700

    feat: render all padel sections in app

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 
+import AboutSection from './components/about-section';
+import CommunitySection from './components/community-section';
 import FeaturesSection from './components/features-section';
 import Footer from './components/footer';
 import HeroSection from './components/hero-section';
 import Navbar from './components/navbar';
 import PricingSection from './components/pricing-section';
 import RootProvider from './components/providers/root';
+import TeamSection from './components/team-section';
 import TestimonialsSection from './components/testimonials-section';
 
 function App() {
@@ -16,9 +19,12 @@ function App() {
           <Navbar />
           <main className='flex-1 flex flex-col'>
             <HeroSection />
+            <AboutSection />
             <FeaturesSection />
             <TestimonialsSection />
+            <CommunitySection />
             <PricingSection />
+            <TeamSection />
           </main>
           <Footer />
         </div>
